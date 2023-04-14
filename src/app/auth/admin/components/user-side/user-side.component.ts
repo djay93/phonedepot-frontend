043d95eac8c8c5bac3dbb378120e790d4428c43f
djay93 <djay93@djay93.com>
2023-04-14 09:01:02 -0400

    initial push

--- src/app/auth/admin/components/user-side/user-side.component.ts
+++ src/app/auth/admin/components/user-side/user-side.component.ts
@@ -56,6 +56,7 @@ export class UserSideComponent implements OnInit, OnChanges {
 
   createUser() {
     this.alterableUser = this.user.value;
+    this.alterableUser.phone = 0;
     this.userService.createUser(this.alterableUser).subscribe(() => {
       this.userPage.ngOnInit();
       this.clean();
@@ -64,6 +65,7 @@ export class UserSideComponent implements OnInit, OnChanges {
 
   editUsers() {
     this.alterableUser = this.user.value;
+    this.alterableUser.phone = 0;
     this.userService.editUsers(this.alterableUser, this.editUser.idUser!).subscribe(() => {
       this.userPage.ngOnInit();
       this.clean();
