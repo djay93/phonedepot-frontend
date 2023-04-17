d96db03e407fa5e69745fb8e20139844ec63c2f0
djay93 <djay93@djay93.com>
2023-04-17 01:42:18 -0400

    initial push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  urlBackendApi: 'http://localhost:8081/api/v1',
-  //urlBackendApi: 'http://k8s-services-cce61f7386-936350103.us-east-1.elb.amazonaws.com/api/v1'
+  //urlBackendApi: 'http://localhost:8081/api/v1',
+  urlBackendApi: 'http://k8s-services-cce61f7386-936350103.us-east-1.elb.amazonaws.com/api/v1'
 };
 
 /*
